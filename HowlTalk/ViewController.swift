5c42b8f173b1a22c0e35e09b9e77f5ebc836943c
SungKihun <tjdrlgns3293@gmail.com>
2021-10-18 21:29:30 +0900

    Initial Commit

---
+++ HowlTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HowlTalk
+//
+//  Created by 성기훈 on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
