bfbffa9af9b1d0c90fb336a99387314d8144fde3
SungKihun <tjdrlgns3293@gmail.com>
2021-10-21 15:04:55 +0900

    로그인 이벤트 구현. 로그인 시 메인화면으로 넘어감

--- HowlTalk/LoginViewController.swift
+++ HowlTalk/LoginViewController.swift
@@ -10,8 +10,11 @@ import Firebase
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var signUp: UIButton!
+    @IBOutlet var email: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var signUp: UIButton!
     
     let remoteConfig = RemoteConfig.remoteConfig()
     var color: String!
@@ -19,6 +22,9 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 미리 로그아웃
+        try! Auth.auth().signOut()
+        
         let statusBar = UIView()
         self.view.addSubview(statusBar)
         statusBar.snp.makeConstraints { make in
@@ -32,7 +38,29 @@ class LoginViewController: UIViewController {
         loginButton.backgroundColor = UIColor(hex: color)
         signUp.backgroundColor = UIColor(hex: color)
         
+        loginButton.addTarget(self, action: #selector(loginEvent), for: .touchUpInside)
+        
         signUp.addTarget(self, action: #selector(presentSignUp), for: .touchUpInside)
+        
+        Auth.auth().addStateDidChangeListener { Auth, User in
+            if User != nil {
+                let view = self.storyboard?.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+                
+                self.present(view, animated: true)
+            }
+        }
+    }
+    
+    @objc func loginEvent() {
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { AuthDataResult, Error in
+            if Error != nil {
+                let alert = UIAlertController(title: "에러", message: Error.debugDescription, preferredStyle: .alert)
+                let ok = UIAlertAction(title: "확인", style: .default)
+                alert.addAction(ok)
+                
+                self.present(alert, animated: true)
+            }
+        }
     }
 
     @objc func presentSignUp() {
