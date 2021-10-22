a706b0acb16eb48cf165ea44f202d9b04859a4d8
SungKihun <tjdrlgns3293@gmail.com>
2021-10-22 11:13:46 +0900

    채팅 메시지 보내기. 채팅방 나갔다 들어와야 코멘트 쌓이는 버그

--- HowlTalk/ChatViewController.swift
+++ HowlTalk/ChatViewController.swift
@@ -9,25 +9,52 @@ import UIKit
 import Firebase
 
 class ChatViewController: UIViewController {
-
+    
+    @IBOutlet var textfield_message: UITextField!
     @IBOutlet var sendButton: UIButton!
+    
+    var uid: String?
+    var chatRoomUid: String?
     
     public var destinationUid: String? // 나중에 내가 채팅할 대상의 uid
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        uid = Auth.auth().currentUser?.uid
         sendButton.addTarget(self, action: #selector(createRoom), for: .touchUpInside)
+        
+        checkChatRoom()
     }
-
-    @objc
-    func createRoom() {
+    
+    @objc func createRoom() {
         let createRoomInfo = [
-            "uid": Auth.auth().currentUser?.uid,
-            "destinationUid": destinationUid
+            "users": [
+                uid!: true,
+                destinationUid!: true
+            ]
         ]
         
-        Database.database().reference().child("chatrooms").childByAutoId().setValue(createRoomInfo)
+        if chatRoomUid == nil {
+            Database.database().reference().child("chatrooms").childByAutoId().setValue(createRoomInfo)
+        } else {
+            let value = [
+                "comments": [
+                    "uid": uid!,
+                    "message": textfield_message.text!
+                ]
+            ]
+            
+            Database.database().reference().child("chatrooms").child(chatRoomUid!).child("comments").childByAutoId().setValue(value)
+        }
+    }
+    
+    func checkChatRoom() {
+        Database.database().reference().child("chatrooms").queryOrdered(byChild: "users/" + uid!).queryEqual(toValue: true).observeSingleEvent(of: DataEventType.value) { DataSnapshot in
+            for item in DataSnapshot.children.allObjects as! [DataSnapshot] {
+                self.chatRoomUid = item.key
+            }
+        }
     }
     
     /*
